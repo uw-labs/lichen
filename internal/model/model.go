e72069227284a22ef9fde9c8f9de5fb29219d71a
Nicholas Jones <me@nicholasjon.es>
2020-07-05 18:13:24 +0100

    Add comments to model package

--- internal/model/model.go
+++ internal/model/model.go
@@ -2,6 +2,7 @@ package model
 
 import "fmt"
 
+// BuildInfo encapsulates build info embedded into a Go compile binary
 type BuildInfo struct {
 	Path        string            // OS level absolute path to the binary this build info relates to
 	PackagePath string            // package path indicated by the build info, e.g. github.com/foo/bar/cmd/baz
@@ -9,21 +10,25 @@ type BuildInfo struct {
 	ModuleRefs  []ModuleReference // all modules that feature in the build info output
 }
 
+// Module carries details of a Go module
 type Module struct {
 	ModuleReference           // reference (path & version)
 	Dir             string    // OS level absolute path to where the cached copy of the module is located
 	Licenses        []License // resolved licenses
 }
 
+// ModuleReference is a reference to a particular version of a named module
 type ModuleReference struct {
 	Path    string // module path, e.g. github.com/foo/bar
 	Version string // module version (can take a variety of forms)
 }
 
+// String returns a typical string representation of a module reference (path@version)
 func (r ModuleReference) String() string {
 	return fmt.Sprintf("%s@%s", r.Path, r.Version)
 }
 
+// License carries license classification details
 type License struct {
 	Name       string  // SPDX name of the license
 	Confidence float64 // confidence from license classification
